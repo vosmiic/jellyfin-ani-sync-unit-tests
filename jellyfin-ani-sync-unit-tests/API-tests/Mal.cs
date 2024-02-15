0297e5613db545107b3096739f04cee740cde691
vosmiic <44092756+vosmiic@users.noreply.github.com>
2024-02-14 20:18:59 -0500

    adding mock http requests to unit tests

--- jellyfin-ani-sync-unit-tests/API-tests/Mal.cs
+++ jellyfin-ani-sync-unit-tests/API-tests/Mal.cs
@@ -1,41 +1,79 @@
 using System;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
-using jellyfin_ani_sync;
+using System.Web.Http;
 using jellyfin_ani_sync.Api;
 using jellyfin_ani_sync.Configuration;
 using jellyfin_ani_sync.Models;
 using jellyfin_ani_sync.Models.Mal;
-using MediaBrowser.Common.Net;
 using MediaBrowser.Controller;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
-using Moq.Contrib.HttpClient;
 using NUnit.Framework;
 
 namespace jellyfin_ani_sync_unit_tests.API_tests;
 
 public class Mal {
     private MalApiCalls _malApiCalls;
+    private ILoggerFactory _loggerFactory;
+    private Mock<IServerApplicationHost> _serverApplicationHost;
+    private Mock<IHttpContextAccessor> _httpContextAccessor;
+    private IHttpClientFactory _httpClientFactory;
+
+    private class DelegatingHandlerStub : DelegatingHandler {
+        private readonly Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> _handlerFunc;
+        public DelegatingHandlerStub(Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> handlerFunc) {
+            _handlerFunc = handlerFunc;
+        }
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken) {
+            return _handlerFunc(request, cancellationToken);
+        }
+    }
+
+    private void MockHttpCalls(HttpStatusCode responseCode, string responseContent) {
+        var mockFactory = new Mock<IHttpClientFactory>();
+        var clientHandlerStub = new DelegatingHandlerStub((request, _) => {
+            request.SetConfiguration(new HttpConfiguration());
+            return Task.FromResult(new HttpResponseMessage(responseCode) { RequestMessage = request, Content = new StringContent(responseContent) });
+        });
+        var client = new HttpClient(clientHandlerStub);
+        mockFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(client);
+        _httpClientFactory = mockFactory.Object;
+    }
 
     [SetUp]
     public void Setup() {
-        var mockFactory = new Mock<IHttpClientFactory>();
-        var client = new HttpClient();
-        mockFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(client);
-        IHttpClientFactory factory = mockFactory.Object;
-
-        var mockLoggerFactory = new NullLoggerFactory();
-        var mockServerApplicationHost = new Mock<IServerApplicationHost>();
-        var mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
-        _malApiCalls = new MalApiCalls(factory, mockLoggerFactory, mockServerApplicationHost.Object, mockHttpContextAccessor.Object, GetUserConfig.ManuallyGetUserConfig());
+        _loggerFactory = new NullLoggerFactory();
+        _serverApplicationHost = new Mock<IServerApplicationHost>();
+        _httpContextAccessor = new Mock<IHttpContextAccessor>();
     }
 
     [Test]
     public async Task TestGenericGetUserInformation() {
+        MockHttpCalls(HttpStatusCode.OK, JsonSerializer.Serialize(new User {
+            Id = 1,
+            Name = "name",
+            Location = "location",
+            JoinedAt = DateTime.UtcNow,
+            Picture = "picture"
+        }));
+
+        _malApiCalls = new MalApiCalls(_httpClientFactory, _loggerFactory, _serverApplicationHost.Object, _httpContextAccessor.Object, new UserConfig {
+            UserApiAuth = new [] {
+                new UserApiAuth {
+                    AccessToken = "accessToken",
+                    Name = ApiName.Mal,
+                    RefreshToken = "refreshToken"
+                }
+            }
+        });
+
         var result = await _malApiCalls.GetUserInformation();
         Assert.IsNotNull(result.Id);
     }
