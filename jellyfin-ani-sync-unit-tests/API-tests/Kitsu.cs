0dd2bd02ac130ce6628a51a0b779975003644185
vosmiic <44092756+vosmiic@users.noreply.github.com>
2024-02-20 22:13:13 -0500

    sorting out kitsu unit tests

--- jellyfin-ani-sync-unit-tests/API-tests/Kitsu.cs
+++ jellyfin-ani-sync-unit-tests/API-tests/Kitsu.cs
@@ -1,10 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
+using jellyfin_ani_sync.Api;
 using jellyfin_ani_sync.Api.Anilist;
 using jellyfin_ani_sync.Api.Kitsu;
+using jellyfin_ani_sync.Configuration;
+using jellyfin_ani_sync.Models;
 using jellyfin_ani_sync.Models.Kitsu;
 using MediaBrowser.Controller;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
 using NUnit.Framework;
@@ -13,29 +21,60 @@ namespace jellyfin_ani_sync_unit_tests.API_tests;
 
 public class Kitsu {
     private KitsuApiCalls _kitsuApiCalls;
+    private ILoggerFactory _loggerFactory;
+    private Mock<IServerApplicationHost> _serverApplicationHost;
+    private Mock<IHttpContextAccessor> _httpContextAccessor;
+    private IHttpClientFactory _httpClientFactory;
 
-    [SetUp]
-    public void Setup() {
-        var mockFactory = new Mock<IHttpClientFactory>();
-        var client = new HttpClient();
-        mockFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(client);
-        IHttpClientFactory factory = mockFactory.Object;
-
-        var mockLoggerFactory = new NullLoggerFactory();
-        var mockServerApplicationHost = new Mock<IServerApplicationHost>();
-        var mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
-        _kitsuApiCalls = new KitsuApiCalls(factory, mockLoggerFactory, mockServerApplicationHost.Object, mockHttpContextAccessor.Object, GetUserConfig.ManuallyGetUserConfig());
+    private void Setup(HttpStatusCode responseCode, string responseContent) {
+        _loggerFactory = new NullLoggerFactory();
+        _serverApplicationHost = new Mock<IServerApplicationHost>();
+        _httpContextAccessor = new Mock<IHttpContextAccessor>();
+        Helpers.MockHttpCalls(responseCode, responseContent, ref _httpClientFactory);
+        _kitsuApiCalls = new KitsuApiCalls(_httpClientFactory, _loggerFactory, _serverApplicationHost.Object, _httpContextAccessor.Object, new UserConfig {
+            UserApiAuth = new [] {
+                new UserApiAuth {
+                    AccessToken = "accessToken",
+                    Name = ApiName.Kitsu,
+                    RefreshToken = "refreshToken"
+                }
+            },
+            KeyPairs = new List<KeyPairs> {
+                new KeyPairs {
+                    Key = "KitsuUserId",
+                    Value = 1.ToString()
+                }
+            }
+        });
     }
     
     [Test]
     public async Task TestGenericSearch() {
-        var result = await _kitsuApiCalls.SearchAnime("K-ON!");
+        Setup(HttpStatusCode.OK, JsonSerializer.Serialize(new KitsuSearch.KitsuSearchMedia {
+            KitsuSearchData = new List<KitsuSearch.KitsuAnime> {
+                new()  {
+                    Id = 1
+                }
+            }
+        }));
+        var result = await _kitsuApiCalls.SearchAnime(String.Empty);
         
         Assert.IsNotNull(result[0].Id);
     }
 
     [Test]
     public async Task TestGetUserInformation() {
+        Setup(HttpStatusCode.OK, JsonSerializer.Serialize(new KitsuGetUser.KitsuUserRoot {
+            KitsuUserList = new List<KitsuGetUser.KitsuUserData> {
+                new () {
+                    Id = 1,
+                    KitsuUser = new KitsuGetUser.KitsuUser {
+                        Id = 2,
+                        Name = String.Empty
+                    }
+                }
+            }
+        }));
         var result = await _kitsuApiCalls.GetUserInformation();
         
         Assert.IsNotNull(result);
@@ -43,36 +82,78 @@ public class Kitsu {
 
     [Test]
     public async Task TestGetAnime() {
-        var result = await _kitsuApiCalls.GetAnime(4240);
+        Setup(HttpStatusCode.OK, JsonSerializer.Serialize(new KitsuGet.KitsuGetAnime {
+            KitsuAnimeData = new KitsuSearch.KitsuAnime {
+                Id = 1
+            }
+        }));
+        var result = await _kitsuApiCalls.GetAnime(1);
         
         Assert.IsNotNull(result.KitsuAnimeData.Id);
     }
 
     [Test]
     public async Task TestGetUserList() {
-        var result = await _kitsuApiCalls.GetUserAnimeStatus(1289093, 11614);
+        Setup(HttpStatusCode.OK, JsonSerializer.Serialize(new KitsuUpdate.KitsuLibraryEntryListRoot {
+            Data = new List<KitsuUpdate.KitsuLibraryEntry> {
+                new () {
+                    Id = 1,
+                    Attributes = new KitsuUpdate.Attributes {
+                        Progress = 1
+                    }
+                }
+            }
+        }));
+        var result = await _kitsuApiCalls.GetUserAnimeStatus(1, 1);
         
         Assert.IsNotNull(result.Attributes.Progress);
     }
 
     [Test]
     public async Task TestUpdateAnimeStatus() {
-        var result = await _kitsuApiCalls.UpdateAnimeStatus(4240, 6, KitsuUpdate.Status.current);
+        Setup(HttpStatusCode.OK, JsonSerializer.Serialize(new KitsuUpdate.KitsuLibraryEntryListRoot {
+            Data = new List<KitsuUpdate.KitsuLibraryEntry> {
+                new () {
+                    Id = 1,
+                    Attributes = new KitsuUpdate.Attributes {
+                        Progress = 1
+                    }
+                }
+            }
+        }));
+        var result = await _kitsuApiCalls.UpdateAnimeStatus(1, 1, KitsuUpdate.Status.current);
         
         Assert.IsTrue(result);
     }
 
     [Test]
     public async Task TestGetRelatedAnime() {
-        var result = await _kitsuApiCalls.GetRelatedAnime(4240);
+        Setup(HttpStatusCode.OK, JsonSerializer.Serialize(new KitsuMediaRelationship.MediaRelationship {
+            Data = new List<KitsuMediaRelationship.RelationshipData> {
+                new () {
+                    Id = 1.ToString(),
+                    Relationships = new KitsuMediaRelationship.Relationships {
+                        Destination = new KitsuMediaRelationship.Destination {
+                            RelationshipData = new KitsuMediaRelationship.RelationshipData {
+                                Id = 1.ToString(),
+                                Type = "anime"
+                            }
+                        }
+                    },
+                    Attributes = new KitsuMediaRelationship.Attributes {
+                        RelationType = KitsuMediaRelationship.RelationType.sequel
+                    }
+                }
+            },
+            Included = new List<KitsuSearch.KitsuAnime> {
+                new () {
+                    Id = 1,
+                    RelationType = KitsuMediaRelationship.RelationType.sequel
+                }
+            }
+        }));
+        var result = await _kitsuApiCalls.GetRelatedAnime(1);
         
         Assert.IsNotNull(result);
-    }
-    
-    [Test]
-    public async Task TestGetRelatedAnimeCombinationWorking() {
-        var result = await _kitsuApiCalls.GetAnime(4240);
-        
-        Assert.IsNotEmpty(result.KitsuAnimeData.RelatedAnime);
     }
 }
