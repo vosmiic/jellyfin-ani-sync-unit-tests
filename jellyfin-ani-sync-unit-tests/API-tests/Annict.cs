48102cd9169722d51a31869e4c6a7ec9b535cffd
vosmiic <44092756+vosmiic@users.noreply.github.com>
2024-02-20 21:25:48 -0500

    sorting out annict unit tests

--- jellyfin-ani-sync-unit-tests/API-tests/Annict.cs
+++ jellyfin-ani-sync-unit-tests/API-tests/Annict.cs
@@ -1,43 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using jellyfin_ani_sync.Api.Anilist;
 using jellyfin_ani_sync.Api.Annict;
+using jellyfin_ani_sync.Configuration;
 using jellyfin_ani_sync.Models;
 using jellyfin_ani_sync.Models.Annict;
-using Jellyfin.Extensions;
 using MediaBrowser.Controller;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
 using NUnit.Framework;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace jellyfin_ani_sync_unit_tests.API_tests; 
 
 public class Annict {
     private AnnictApiCalls _annictApiCalls;
+    private ILoggerFactory _loggerFactory;
+    private Mock<IServerApplicationHost> _serverApplicationHost;
+    private Mock<IHttpContextAccessor> _httpContextAccessor;
+    private IHttpClientFactory _httpClientFactory;
 
-    [SetUp]
-    public void Setup() {
-        var mockFactory = new Mock<IHttpClientFactory>();
-        var client = new HttpClient();
-        mockFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(client);
-        IHttpClientFactory factory = mockFactory.Object;
-
-        var mockLoggerFactory = new NullLoggerFactory();
-        var mockServerApplicationHost = new Mock<IServerApplicationHost>();
-        var mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
-        _annictApiCalls = new AnnictApiCalls(factory, mockLoggerFactory, mockServerApplicationHost.Object, mockHttpContextAccessor.Object, GetUserConfig.ManuallyGetUserConfig());
+    private void Setup(HttpStatusCode responseCode, string responseContent) {
+        _loggerFactory = new NullLoggerFactory();
+        _serverApplicationHost = new Mock<IServerApplicationHost>();
+        _httpContextAccessor = new Mock<IHttpContextAccessor>();
+        Helpers.MockHttpCalls(responseCode, responseContent, ref _httpClientFactory);
+        _annictApiCalls = new AnnictApiCalls(_httpClientFactory, _loggerFactory, _serverApplicationHost.Object, _httpContextAccessor.Object, new UserConfig {
+            UserApiAuth = new [] {
+                new UserApiAuth {
+                    AccessToken = "accessToken",
+                    Name = ApiName.Annict,
+                    RefreshToken = "refreshToken"
+                }
+            }
+        });
     }
 
     [Test]
     public async Task TestGenericSearch() {
-        var result = await _annictApiCalls.SearchAnime("けいおん");
+        Setup(HttpStatusCode.OK, JsonSerializer.Serialize(new AnnictSearch.AnnictSearchMedia {
+            AnnictSearchData = new AnnictSearch.AnnictSearchData {
+                SearchWorks = new AnnictSearch.SearchWorks {
+                    Nodes = new List<AnnictSearch.AnnictAnime> {
+                        new()  {
+                            Id = String.Empty
+                        }
+                    }
+                }
+            }
+        }));
+        var result = await _annictApiCalls.SearchAnime(String.Empty);
         
-        Assert.IsNotNull(result[0].Id);
+        Assert.IsNotNull(result?[0]?.Id);
     }
 
     [Test]
     public async Task TestGettingCurrentUser() {
+        Setup(HttpStatusCode.OK, JsonSerializer.Serialize(new AnnictViewer.AnnictViewerRoot {
+            AnnictSearchData = new AnnictViewer.AnnictViewerData {
+                Viewer = new AnnictViewer.AnnictViewerDetails {
+                    username = String.Empty
+                }
+            }
+        }));
         var result = await _annictApiCalls.GetCurrentUser();
         
         Assert.IsNotNull(result);
@@ -45,6 +74,22 @@ public class Annict {
     
     [Test]
     public async Task TestGettingUserAnimeList() {
+        Setup(HttpStatusCode.OK, JsonSerializer.Serialize(new AnnictMediaList.AnnictUserMediaList {
+            AnnictSearchData = new AnnictMediaList.AnnictUserMediaListData {
+                Viewer = new AnnictMediaList.AnnictUserMediaListViewer {
+                    AnnictUserMediaListLibraryEntries = new AnnictMediaList.AnnictUserMediaListLibraryEntries {
+                        Nodes = new List<AnnictSearch.AnnictAnime> {
+                            new()  {
+                                Id = String.Empty
+                            }
+                        },
+                        PageInfo = new AnnictMediaList.PageInfo {
+                            HasNextPage = false
+                        }
+                    }
+                }
+            }
+        }));
         var result = await _annictApiCalls.GetAnimeList(AnnictSearch.AnnictMediaStatus.Watched);
         
         Assert.IsNotNull(result);
@@ -52,6 +97,7 @@ public class Annict {
 
     [Test]
     public async Task TestUpdatingAnime() {
+        Setup(HttpStatusCode.OK, String.Empty);
         var result = await _annictApiCalls.UpdateAnime("V29yay02Njg=", AnnictSearch.AnnictMediaStatus.Watched);
         
         Assert.IsTrue(result);
