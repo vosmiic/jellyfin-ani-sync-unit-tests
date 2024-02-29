38ddf9df899a0e9c35485a3fe17abb17a08ace01
vosmiic <44092756+vosmiic@users.noreply.github.com>
2024-02-28 21:21:27 -0500

    updating anilist tests

--- jellyfin-ani-sync-unit-tests/API-tests/AniList.cs
+++ jellyfin-ani-sync-unit-tests/API-tests/AniList.cs
@@ -14,7 +14,7 @@ using Moq;
 using NUnit.Framework;
 using JsonSerializer = System.Text.Json.JsonSerializer;
 
-namespace jellyfin_ani_sync_unit_tests.API_tests; 
+namespace jellyfin_ani_sync_unit_tests.API_tests;
 
 public class AniList {
     private AniListApiCalls _aniListApiCalls;
@@ -23,11 +23,11 @@ public class AniList {
     private Mock<IHttpContextAccessor> _httpContextAccessor;
     private IHttpClientFactory _httpClientFactory;
 
-    private void Setup(HttpStatusCode responseCode, string responseContent) {
+    private void Setup(List<Helpers.HttpCall> httpCalls) {
         _loggerFactory = new NullLoggerFactory();
         _serverApplicationHost = new Mock<IServerApplicationHost>();
         _httpContextAccessor = new Mock<IHttpContextAccessor>();
-        Helpers.MockHttpCalls(responseCode, responseContent, ref _httpClientFactory);
+        Helpers.MockHttpCalls(httpCalls, ref _httpClientFactory);
         _aniListApiCalls = new AniListApiCalls(_httpClientFactory, _loggerFactory, _serverApplicationHost.Object, _httpContextAccessor.Object, new UserConfig {
             UserApiAuth = new [] {
                 new UserApiAuth {
@@ -41,82 +41,118 @@ public class AniList {
 
     [Test]
     public async Task TestGenericSearch() {
-        Setup(HttpStatusCode.OK, JsonSerializer.Serialize(new AniListSearch.AniListSearchMedia {
-            Data = new AniListSearch.AniListSearchData {
-                Page = new AniListSearch.Page {
-                    Media = new List<AniListSearch.Media> {
-                        new()  {
-                            Id = 1
+        Setup(new List<Helpers.HttpCall> {
+            new () {
+                RequestMethod = HttpMethod.Post,
+                RequestUrlMatch = url => url.Contains("anilist"),
+                ResponseCode = HttpStatusCode.OK,
+                ResponseContent = JsonSerializer.Serialize(new AniListSearch.AniListSearchMedia {
+                    Data = new AniListSearch.AniListSearchData {
+                        Page = new AniListSearch.Page {
+                            Media = new List<AniListSearch.Media> {
+                                new()  {
+                                    Id = 1
+                                }
+                            },
+                            PageInfo = new AniListSearch.PageInfo {
+                                HasNextPage = false
+                            }
                         }
-                    },
-                    PageInfo = new AniListSearch.PageInfo {
-                        HasNextPage = false
                     }
-                }
+                })
             }
-        }));
+        });
+
         var result = await _aniListApiCalls.SearchAnime(String.Empty);
-        
+
         Assert.IsNotNull(result[0].Id);
     }
 
     [Test]
     public async Task TestGettingCurrentUser() {
-        Setup(HttpStatusCode.OK, JsonSerializer.Serialize(new AniListViewer.AniListGetViewer {
-            Data = new AniListViewer.AniListViewerData {
-                Viewer = new AniListViewer.Viewer {
-                    Id = 1
-                }
+        Setup(new List<Helpers.HttpCall> {
+            new () {
+                RequestMethod = HttpMethod.Post,
+                RequestUrlMatch = url => url.Contains("anilist"),
+                ResponseCode = HttpStatusCode.OK,
+                ResponseContent = JsonSerializer.Serialize(new AniListViewer.AniListGetViewer {
+                    Data = new AniListViewer.AniListViewerData {
+                        Viewer = new AniListViewer.Viewer {
+                            Id = 1
+                        }
+                    }
+                })
             }
-        }));
+        });
         var result = await _aniListApiCalls.GetCurrentUser();
-        
+
         Assert.IsNotNull(result);
     }
 
     [Test]
     public async Task TestUpdatingAnime() {
-        Setup(HttpStatusCode.OK, String.Empty);
+        Setup(new List<Helpers.HttpCall> {
+            new () {
+                RequestMethod = HttpMethod.Post,
+                RequestUrlMatch = url => url.Contains("anilist"),
+                ResponseCode = HttpStatusCode.OK,
+                ResponseContent = String.Empty
+            }
+        });
         var result = await _aniListApiCalls.UpdateAnime(1,
             AniListSearch.MediaListStatus.Current,
             1,
             numberOfTimesRewatched: 1,
             startDate: DateTime.UtcNow - TimeSpan.FromHours(1),
             endDate: DateTime.UtcNow);
-        
+
         Assert.IsTrue(result);
     }
 
     [Test]
     public async Task TestGenericSearchPaging() {
-        Setup(HttpStatusCode.OK, JsonSerializer.Serialize(new AniListSearch.AniListSearchMedia {
-            Data = new AniListSearch.AniListSearchData {
-                Page = new AniListSearch.Page {
-                    Media = new List<AniListSearch.Media> {
-                        new()  {
-                            Id = 1
+        Setup(new List<Helpers.HttpCall> {
+            new () {
+                RequestMethod = HttpMethod.Post,
+                RequestUrlMatch = url => url.Contains("anilist"),
+                ResponseCode = HttpStatusCode.OK,
+                ResponseContent = JsonSerializer.Serialize(new AniListSearch.AniListSearchMedia {
+                    Data = new AniListSearch.AniListSearchData {
+                        Page = new AniListSearch.Page {
+                            Media = new List<AniListSearch.Media> {
+                                new()  {
+                                    Id = 1
+                                }
+                            },
+                            PageInfo = new AniListSearch.PageInfo {
+                                HasNextPage = true
+                            }
                         }
-                    },
-                    PageInfo = new AniListSearch.PageInfo {
-                        HasNextPage = true
                     }
-                }
+                })
             }
-        }));
+        });
         var result = await _aniListApiCalls.SearchAnime(String.Empty);
-        
+
         Assert.IsTrue(result.Count == 10);
     }
 
     [Test]
     public async Task TestGetAnime() {
-        Setup(HttpStatusCode.OK, JsonSerializer.Serialize(new AniListGet.AniListGetMedia {
-            Data = new AniListGet.AniListGetData {
-                Media = new AniListSearch.Media {
-                    Id = 1
-                }
+        Setup(new List<Helpers.HttpCall> {
+            new () {
+                RequestMethod = HttpMethod.Post,
+                RequestUrlMatch = url => url.Contains("anilist"),
+                ResponseCode = HttpStatusCode.OK,
+                ResponseContent = JsonSerializer.Serialize(new AniListGet.AniListGetMedia {
+                    Data = new AniListGet.AniListGetData {
+                        Media = new AniListSearch.Media {
+                            Id = 1
+                        }
+                    }
+                })
             }
-        }));
+        });
         var result = await _aniListApiCalls.GetAnime(1);
 
         Assert.IsNotNull(result.Id);
